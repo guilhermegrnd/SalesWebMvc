d82814e882f7e0ddacd0c8a48357f3e6166516b7
guilhermegrnd <guilhermegranado@outlook.com>
2021-04-10 21:58:39 -0300

    Added Seller Edit Function

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services {
     public class SellerService {
@@ -22,13 +24,26 @@ namespace SalesWebMvc.Services {
         }
 
         public Seller FindById(int id) {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id) {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj) {
+            if(!_context.Seller.Any(x => x.Id == obj.Id)) {
+                throw new NotFoundException("Id not Found");
+            }
+            try {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e) {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
