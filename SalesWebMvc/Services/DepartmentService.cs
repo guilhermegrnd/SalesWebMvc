82186c7f6e4b305e6ea225ce893f5ae8bdc107a5
guilhermegrnd <guilhermegranado@outlook.com>
2021-04-12 23:21:07 -0300

    Change dbcontext to async functions

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services {
     public class DepartmentService {
@@ -11,8 +12,8 @@ namespace SalesWebMvc.Services {
         public DepartmentService(SalesWebMvcContext context) {
             _context = context;
         }
-        public List<Department> FindAll() {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+        public async Task<List<Department>> FindAllAsync() {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
